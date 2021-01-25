5842215ab47327156b6e4b4589c20b27cb803a13
hq <www.657473650@qq.com>
2021-01-25 23:21:17 +0800

    0

---
+++ src/main/java/com/hq/kuwanbackend/KuwanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hq.kuwanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuwanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuwanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hq/kuwanbackend/KuwanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hq.kuwanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuwanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
